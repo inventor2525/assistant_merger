29e1599a56d6af7d57126e39bb03a2b4cf6d165f
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2025-06-09 00:48:14 -0400

    + More tests

---
+++ tests/examples/manual_tests/expected_output/vector3_7.py
@@ -0,0 +1,34 @@
+# Vector3 class for 3D vector operations
+# Version 1: Basic implementation
+# Author: Charlie
+
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def add(self, other):
+        # Optimized addition
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def scale(self, factor):
+        return Vector3(self.x * factor, self.y * factor, self.z * factor)
+
+    def normalize(self):
+        mag = self.magnitude()
+        if mag == 0:
+            return Vector3(0, 0, 0)
+        return Vector3(self.x / mag, self.y / mag, self.z / mag)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def cross(self, other):
+        return Vector3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x
+        )
+
+# End of Vector3 implementation
---
+++ tests/examples/manual_tests/expected_output/vector3_8.py
@@ -0,0 +1,31 @@
+
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def add(self, other):
+        # Optimized addition
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def scale(self, factor):
+        return Vector3(self.x * factor, self.y * factor, self.z * factor)
+
+    def normalize(self):
+        mag = self.magnitude()
+        if mag == 0:
+            return Vector3(0, 0, 0)
+        return Vector3(self.x / mag, self.y / mag, self.z / mag)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def cross(self, other):
+        return Vector3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x
+        )
+
+# End of Vector3 implementation
