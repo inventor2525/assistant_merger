1c39968f96bf887ab6e8e74f524d0e957c9ad16d
Charlie Angela Mehlenbeck <charlie_inventor2003@yahoo.com>
2025-10-05 21:11:04 -0400

    proper setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="assistant_merger",
     version="0.1.0",
-    packages=["assistant_merger"],
-    install_requires=["gitpython>=3.1.0"],
-    description="A tool to interact with git diffs for large language model review",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Charlie Mehlenbeck",
+    author_email="charlie_inventor2003@yahoo.com",
+    description="A tool that facilitates interaction with git diffs that can aid LLMs in reviewing their own code changes in detail.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/inventor2525/assistant_merger",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires=">=3.8",
+    install_requires=[],
+    extras_require={
+        "dev": ["unittest"],
+    },
 )
